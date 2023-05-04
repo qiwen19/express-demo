601e36af782a0afaeb3857bb56cebdaf2855c1a2
dudu-bubu-admin <qiwen.oq@gmail.com>
2023-05-04 15:01:35 +0800

    added Joi validation, post put delete function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -15,11 +18,77 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
-app.post()
+app.post('/api/courses', (req, res) => {
+    // const schema = {
+    //     name: Joi.string().min(3).required()
+    // };
+
+    // const result = Joi.validate(req.body, schema);
+    // if (result.error) {
+    //     // 400 Bad Request
+    //     res.status(400).send(result.error.details[0].message);
+    //     return;
+    // }
+    const { error } = validateCourse(req.body); // result.error
+    if (error) return res.status(400).send(error.details[0].message); // 400 Bad Request
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course)
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send('The course with the given ID was not found.');
+    }
+
+    // Validate
+    // If invalidate, return 400 - Bad request
+    const { error } = validateCourse(req.body); // result.error
+    if (error) {
+        // 400 Bad Request
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Update course
+    // return the updated course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // Not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send('The course with the given ID was not found.');
+    }
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // return the same course
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.');
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
     res.send(course);
     // res.send(req.query);
 });
